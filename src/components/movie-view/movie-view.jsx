ecdb3ae3384d4585326a1afc7e3159636d2d68ab
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-04-24 15:30:19 +0200

    feat: add MovieView component

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+export class MovieView extends React.Component {
+  render() {
+    const { movieData, onBackClick } = this.props;
+
+    return (
+      <div className='movie-view'>
+        <div className='movie-image'>
+          <img src={movieData.imagePath} />
+        </div>
+        <div className='movie-title'>
+          <span className='label'>Title: </span>
+          <span className='value'>{movieData.title}</span>
+        </div>
+        <div className='movie-year'>
+          <span className='label'>Release Year: </span>
+          <span className='value'>{movieData.releaseYear}</span>
+        </div>
+        <div className='movie-genres'>
+          <span className='label'>Genres: </span>
+          <span className='value'>{movieData.genre.join(' / ')}</span>
+        </div>
+        <div className='movie-director'>
+          <span className='label'>Director: </span>
+          <span className='value'>{movieData.director}</span>
+        </div>
+        <div className='movie-actors'>
+          <span className='label'>Main Cast: </span>
+          <span className='value'>{movieData.actors.join(', ')}</span>
+        </div>
+        <div className='movie-time'>
+          <span className='label'>Run Time: </span>
+          <span className='value'>{movieData.runTime}</span>
+        </div>
+        <div className='movie-description'>
+          <span className='label'>Description: </span>
+          <span className='value'>{movieData.description}</span>
+        </div>
+        <button
+          onClick={() => {
+            onBackClick(null);
+          }}>
+          Back
+        </button>
+      </div>
+    );
+  }
+}
