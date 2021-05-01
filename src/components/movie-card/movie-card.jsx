67dbd056da4967e5c6ae42ae3268c6efc0b96f46
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-05-01 23:58:17 +0200

    perf: improve the structure of the component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap';
 
 export class MovieCard extends React.Component {
   render() {
-    const { movieData, onMovieClick } = this.props;
+    const { movie } = this.props;
 
     return (
-      <Card className='h-100' border='primary'>
-        <Card.Img className='p-2' variant='top' src={movieData.imagePath} />
+      <Card className='h-100'>
+        <Card.Img className='p-2' variant='top' src={movie.imagePath} />
         <Card.Body>
-          <Card.Title className='text-center'>{movieData.title}</Card.Title>
-          <Card.Text className='text-center'>{movieData.releaseYear}</Card.Text>
-          <Card.Text className='font-italic'>{movieData.description}</Card.Text>
+          <Card.Title className='text-center'>{movie.title}</Card.Title>
+          <Card.Text className='text-center'>{movie.releaseYear}</Card.Text>
+          <Card.Text className='font-italic'>{movie.description}</Card.Text>
         </Card.Body>
         <Card.Footer className='text-center'>
-          <Button onClick={() => onMovieClick(movieData)}>Read more</Button>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant='outline-info'>Show more</Button>
+          </Link>
         </Card.Footer>
       </Card>
     );
@@ -24,11 +26,11 @@ export class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  movieData: PropTypes.shape({
+  movie: PropTypes.shape({
     imagePath: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     releaseYear: PropTypes.number.isRequired,
     description: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,66 +1,141 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Row, Col, Button } from 'react-bootstrap';
 
-export class MovieView extends React.Component {
+export default class MovieView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: [],
+      genres: [],
+      directors: [],
+      actors: [],
+    };
+  }
+
+  async componentDidMount() {
+    const { movieID } = this.props.match.params;
+    const token = localStorage.getItem('token');
+
+    const movieURL = 'https://myvhs.herokuapp.com/movies/';
+    const genresURL = 'https://myvhs.herokuapp.com/genres';
+    const directorsURL = 'https://myvhs.herokuapp.com/directors';
+    const actorsURL = 'https://myvhs.herokuapp.com/actors';
+
+    const movieResponse = axios.get(movieURL + movieID, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const genresResponse = axios.get(genresURL, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const directorsResponse = axios.get(directorsURL, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const actorsResponse = axios.get(actorsURL, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const [movie, genres, directors, actors] = await axios.all([
+      movieResponse,
+      genresResponse,
+      directorsResponse,
+      actorsResponse,
+    ]);
+
+    this.setState({
+      movie: movie.data,
+      genres: genres.data,
+      directors: directors.data,
+      actors: actors.data,
+    });
+  }
+
+  getGenreName(id) {
+    const genre = this.state.genres.find((genre) => genre._id === id);
+    return genre.name;
+  }
+
+  getDirectorName(id) {
+    const director = this.state.directors.find(
+      (director) => director._id === id,
+    );
+    return director?.name;
+  }
+
+  getActorName(id) {
+    const actor = this.state.actors.find((actor) => actor._id === id);
+    return actor.name;
+  }
+
   render() {
-    const { movieData, onBackClick } = this.props;
+    let { movie } = this.state;
 
     return (
-      <div className='movie-view text-center'>
-        <div className='movie-image mb-4'>
-          <img src={movieData.imagePath} />
-        </div>
-        <div className='movie-title mb-2'>
-          <span className='label font-weight-bold'>Title: </span>
-          <span className='value'>{movieData.title}</span>
-        </div>
-        <div className='movie-year mb-2'>
-          <span className='label font-weight-bold'>Release Year: </span>
-          <span className='value'>{movieData.releaseYear}</span>
-        </div>
-        <div className='movie-genres mb-2'>
-          <span className='label font-weight-bold'>Genres: </span>
-          <span className='value'>{movieData.genre.join(' / ')}</span>
-        </div>
-        <div className='movie-director mb-2'>
-          <span className='label font-weight-bold'>Director: </span>
-          <span className='value'>{movieData.director}</span>
-        </div>
-        <div className='movie-actors mb-2'>
-          <span className='label font-weight-bold'>Main Cast: </span>
-          <span className='value'>{movieData.actors.join(', ')}</span>
-        </div>
-        <div className='movie-time mb-2'>
-          <span className='label font-weight-bold'>Run Time: </span>
-          <span className='value'>{movieData.runTime}</span>
-        </div>
-        <div className='movie-description mb-4'>
-          <span className='label font-weight-bold'>Description: </span>
-          <span className='value font-italic'>{movieData.description}</span>
-        </div>
-        <Button
-          className='px-5'
-          onClick={() => {
-            onBackClick(null);
-          }}>
-          Back
-        </Button>
-      </div>
+      movie && (
+        <Row className='main-view justify-content-md-center pt-4'>
+          <Col className='movie-view text-center' md={8}>
+            <div className='movie-image mb-4'>
+              <img src={movie.imagePath} width={250} />
+            </div>
+            <div className='movie-title mb-2'>
+              <span className='label font-weight-bold'>Title: </span>
+              <span className='value'>{movie.title}</span>
+            </div>
+            <div className='movie-year mb-2'>
+              <span className='label font-weight-bold'>Release Year: </span>
+              <span className='value'>{movie.releaseYear}</span>
+            </div>
+            <div className='movie-time mb-2'>
+              <span className='label font-weight-bold'>Run Time: </span>
+              <span className='value'>{movie.runTime}</span>
+            </div>
+            <div className='movie-genres mb-2'>
+              <p className='label font-weight-bold mb-1'>Genres: </p>
+              {movie.genre &&
+                movie.genre.map((genre, index) => (
+                  <Link key={index} to={`/genres/${genre}`}>
+                    <Button className='mx-1' variant='outline-info' size='sm'>
+                      {this.getGenreName(genre)}
+                    </Button>
+                  </Link>
+                ))}
+            </div>
+            <div className='movie-director mb-2'>
+              <p className='label font-weight-bold mb-1'>Director: </p>
+              <Link to={`/directors/${movie.director}`}>
+                <Button className='mx-1' variant='outline-info' size='sm'>
+                  {this.getDirectorName(movie.director)}
+                </Button>
+              </Link>
+            </div>
+            <div className='movie-actors mb-2'>
+              <p className='label font-weight-bold mb-1'>Main Cast: </p>
+              {movie.actors &&
+                movie.actors.map((actor, index) => (
+                  <Link key={index} to={`/actors/${actor}`}>
+                    <Button className='mx-1' variant='outline-info' size='sm'>
+                      {this.getActorName(actor)}
+                    </Button>
+                  </Link>
+                ))}
+            </div>
+            <div className='movie-description mb-4'>
+              <span className='label font-weight-bold'>Description: </span>
+              <span className='value font-italic'>{movie.description}</span>
+            </div>
+          </Col>
+        </Row>
+      )
     );
   }
 }
 
 MovieView.propTypes = {
-  movieData: PropTypes.shape({
-    imagePath: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    releaseYear: PropTypes.number.isRequired,
-    genre: PropTypes.array.isRequired,
-    director: PropTypes.string.isRequired,
-    actors: PropTypes.array.isRequired,
-    runTime: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-  }).isRequired,
-  onBackClick: PropTypes.func.isRequired,
+  match: PropTypes.object,
 };
