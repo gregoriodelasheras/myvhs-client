e75bac11554d37aa0697931faecefe8b8a69c256
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-05-01 23:53:05 +0200

    feat: add footer component

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
+import './footer.scss';
+
+export default function Footer() {
+  return (
+    <div className='main-footer fixed-bottom bg-dark pt-3'>
+      <Container>
+        <Row>
+          <Col className='text-center'>
+            <p>
+              Made with ❤ by{' '}
+              <a
+                href='https://gregoriodelasheras.github.io/'
+                target='_blank'
+                rel='noreferrer'
+                className='link-light'>
+                Francisco Gregorio de las Heras
+              </a>
+            </p>
+            <p>
+              Latest version:{' '}
+              <a
+                href='https://github.com/gregoriodelasheras/myVHS-client/releases/latest'
+                target='_blank'
+                rel='noreferrer'>
+                v.1.0.3
+              </a>
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
