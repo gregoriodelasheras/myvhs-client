6836c6c96d2a588673b5195e51bf39124ba9bc1f
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-06-02 19:47:26 +0200

    feat: add new reducers for genres, directors, actors and user

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux';
-import { SET_FILTER, SET_MOVIES } from '../actions/actions';
-
-function visibilityFilter(state = '', action) {
-  switch (action.type) {
-    case SET_FILTER:
-      return action.value;
-    default:
-      return state;
-  }
-}
+import {
+  SET_MOVIES,
+  SET_GENRES,
+  SET_DIRECTORS,
+  SET_ACTORS,
+  SET_FILTER,
+  SET_USER,
+} from '../actions/actions';
 
 function movies(state = [], action) {
   switch (action.type) {
@@ -19,9 +17,58 @@ function movies(state = [], action) {
   }
 }
 
+function genres(state = [], action) {
+  switch (action.type) {
+    case SET_GENRES:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+function directors(state = [], action) {
+  switch (action.type) {
+    case SET_DIRECTORS:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+function actors(state = [], action) {
+  switch (action.type) {
+    case SET_ACTORS:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+function visibilityFilter(state = '', action) {
+  switch (action.type) {
+    case SET_FILTER:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+function user(state = [], action) {
+  switch (action.type) {
+    case SET_USER:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
 const moviesApp = combineReducers({
-  visibilityFilter,
   movies,
+  genres,
+  directors,
+  actors,
+  visibilityFilter,
+  user,
 });
 
 export default moviesApp;
