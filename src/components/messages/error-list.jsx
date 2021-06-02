df0d035fc13f0df6bbdb296029c91a3abbfcf671
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-06-02 19:51:22 +0200

    refactor: add error message for lists (Redux)

---
+++ src/components/messages/error-list.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function ErrorList() {
+  return (
+    <div>
+      <h2>&quot;Houston, we have a problem!&quot;</h2>
+      <p className='h4'>We are working on it</p>
+      <p>
+        Sorry, an unexpected error occurred during the update. Our technical
+        team is already working on it from our moonbase.
+      </p>
+      <p>Please try again later.</p>
+    </div>
+  );
+}
